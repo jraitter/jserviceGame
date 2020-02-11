89940d0e2ad172ab6d38998097404ee85c658ac2
jraitter <jraitter@cableone.net>
2020-02-10 21:48:37 -0700

    first functionality

--- app/Controllers/GameController.js
+++ app/Controllers/GameController.js
@@ -1,13 +1,23 @@
 import GameService from "../Services/GameService.js";
 import store from "../store.js";
 
+let categoryElem = document.getElementById("category");
+let valueElem = document.getElementById("value");
+let questionElem = document.getElementById("question");
+let answerElem = document.getElementById("answer");
+let scoreElem = document.getElementById("score");
 //Private
 function _draw() {
-  let gamedata = store.State.gamedata;
-  console.log("gamedata from controller", gamedata);
-  let template = ""
-  template = gamedata.Template;
-  document.getElementById("game-data").innerHTML = template;
+  let currData = store.State.gamedata;
+  console.log("gamedata from controller", currData);
+  categoryElem.innerText = currData.category;
+  valueElem.innerText = currData.value.toString();
+  questionElem.innerText = currData.question;
+  answerElem.innerText = currData.answer;
+  answerElem.style.visibility = "hidden";
+  // categoryElem.innerText = currData.category;
+
+
 }
 
 //Public
@@ -17,5 +27,14 @@ export default class GameController {
   }
   getApiGamedata() {
     GameService.getApiGamedata();
+  }
+  showAnswer() {
+    console.log("showAnswer method called");
+    if (answerElem.style.visibility == "hidden") {
+      answerElem.style.visibility = "visible";
+    } else {
+      answerElem.style.visibility = "hidden";
+    }
+    // _draw();
   }
 }
